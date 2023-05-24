2cd05ded49e1e79c3c0fd85446e4012c77c395b8
malle <johanna.malle@slf.ch>
2023-05-24 15:46:05 +0200

    clean-up

--- raster_prep.py
+++ raster_prep.py
@@ -13,69 +13,7 @@ import shutil
 import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-def raster2array(geotif_file):
-    """
-    Convert raster to array
-
-    Parameters
-    ==========
-    geotif_file : path to .tif
-        geotif of CHM
-
-    Returns
-    =======
-    asp_array : array
-        new array of all points in .tif
-    chm_array_metadata: string
-        all metadata used to convert raster2array
-    """
-
-    metadata = {}
-    dataset = gdal.Open(geotif_file)
-    metadata['array_rows'] = dataset.RasterYSize
-    metadata['array_cols'] = dataset.RasterXSize
-    metadata['bands'] = dataset.RasterCount
-    metadata['driver'] = dataset.GetDriver().LongName
-    metadata['projection'] = dataset.GetProjection()
-    metadata['gt_ch2018'] = dataset.GetGeoTransform()
-
-    mapinfo = dataset.GetGeoTransform()
-    metadata['pixelWidth'] = mapinfo[1]
-    metadata['pixelHeight'] = mapinfo[5]
-
-    metadata['ext_dict'] = {}
-    metadata['ext_dict']['xMin'] = mapinfo[0]
-    metadata['ext_dict']['xMax'] = mapinfo[0] + dataset.RasterXSize*mapinfo[1]
-    metadata['ext_dict']['yMin'] = mapinfo[3] + dataset.RasterYSize*mapinfo[5]
-    metadata['ext_dict']['yMax'] = mapinfo[3]
-
-    metadata['extent'] = (metadata['ext_dict']['xMin'], metadata['ext_dict']['xMax'],
-                          metadata['ext_dict']['yMin'], metadata['ext_dict']['yMax'])
-
-    if metadata['bands'] == 1:
-        raster = dataset.GetRasterBand(1)
-        metadata['noDataValue'] = raster.GetNoDataValue()
-        metadata['scaleFactor'] = raster.GetScale()
-
-        # band statistics
-        metadata['bandstats'] = {}  # make a nested dictionary to store band stats in same
-        stats = raster.GetStatistics(True, True)
-        metadata['bandstats']['min'] = round(stats[0], 2)
-        metadata['bandstats']['max'] = round(stats[1], 2)
-        metadata['bandstats']['mean'] = round(stats[2], 2)
-        metadata['bandstats']['stdev'] = round(stats[3], 2)
-
-        array = dataset.GetRasterBand(1).ReadAsArray(0, 0, metadata['array_cols'],
-                                                     metadata['array_rows']).astype(float)
-        array[array == (metadata['noDataValue'])] = np.nan
-        array = array/metadata['scaleFactor']
-        return array, metadata
-
-    elif metadata['bands'] > 1:
-        print('More than one band ... need to modify function for case of multiple bands')
-
+from src.help_functions import raster2array
 
 # somehow without this it can't find the proj.db file...
 os.environ['PROJ_LIB'] = '/home/malle/miniconda3/envs/SetUp_sites/share/proj'
